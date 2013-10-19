c781f9011fa696b6c266c04ddc1782b0fd6be558
Fumiaki MATSUSHIMA <mtsmfm@gmail.com>
2013-10-19 20:26:51 +0900

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PokePartyMaker::Application.config.secret_key_base = '550e970a05ad6327e6de48dfd061535e1499d16a4ad0964abfbf05280b995d605d6d74b1b9eebf558895bf9684f1929e603e2a22797fd3813c70c696b9203def'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+PokePartyMaker::Application.config.session_store :cookie_store, key: '_poke_party_maker_session'

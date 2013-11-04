ec524152087e8b315a3cc248f72ebf1dea75de49
Fumiaki MATSUSHIMA <mtsmfm@gmail.com>
2013-11-04 22:41:43 +0900

    wip

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+  def nav_li_link_to(text, path)
+    klass = :active if current_page? path
+    content_tag(:li, class: klass) do
+      link_to text, path
+    end
+  end
 end
